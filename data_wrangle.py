af744b0dd26205ef7da664dd1a0fa99ced2c2e9d
DJimmie <dowdjimmie10@gmail.com>
2019-01-15 06:24:44 -0600

    1st commit of these files

---
+++ data_wrangle.py
@@ -0,0 +1,206 @@
+
+# coding: utf-8
+
+# In[223]:
+
+
+import pandas as pd
+from pandas import set_option
+from io import StringIO
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+plt.style.use('ggplot')
+from pandas.plotting import scatter_matrix
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+pd.options.display.precision=2
+plt.style.use('ggplot')
+import sys
+import sqlite3
+
+
+# In[ ]:
+
+
+class DataWorld():
+    """THIS IS THE PARENT CLASS FOR THE DATA TO BE STUDIED. IT'S PRIMARY PURPOSE IS TO RETREIVE THE SOURCE DATA AND ASSIGN IT
+    AS A PANDAS DATAFRAME (the_data)"""
+    
+#     file_path=r'C:\Users\Crystal\Desktop\Programs\stock_analysis\daily_scan_data'
+    
+    def __init__(self):
+        self.file_path='Enter the source file path'
+        self.what='WTF'
+        
+    def get_data(self,source_file,source_file_type):
+        """RETRIEVING THE DATA in CSV FORMAT"""
+        self.source_file=source_file
+        self.source_file_type=source_file_type
+        self.source_file_name=self.source_file+'.'+self.source_file_type
+#         self.location=DataWorld.file_path+'\\'+self.file_name
+        self.location=self.file_path+'\\'+self.source_file_name
+        
+        if (self.source_file_type=='csv'):
+            self.the_data=pd.read_csv(self.location,parse_dates=True)
+            
+        elif(self.source_file_type=='sqlite3' or self.source_file_type=='db'):
+            conn=sqlite3.connect(self.location)
+            cur=conn.cursor()
+            cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+            self.table_list=cur.fetchall()
+            print(self.table_list)
+            
+            self.list_of_tables=[]
+            for i in range(len(self.table_list)):
+                print(self.table_list[i][0])
+                self.list_of_tables.append(self.table_list[i][0])
+            print(self.list_of_tables)
+
+            self.the_data=pd.read_sql_query("SELECT * FROM "+self.list_of_tables[0],conn)
+            cur.close()
+            conn.close()
+            
+        pd.options.display.precision=2
+
+
+
+    def look_at_SQL(self,sql_table,the_limit=5):
+        
+        conn=sqlite3.connect(self.location)
+        cur=conn.cursor()
+        self.the_data=pd.read_sql_query("SELECT * FROM "+sql_table,conn)
+
+        cur.close()
+        conn.close()
+        
+      
+#     @classmethod
+#     def specify_path(cls,new_path):
+#         cls.file_path=new_path
+        
+    
+    def view_data(self):
+        print(self.the_data.info(),'\n')
+        print('Checking for null values: \n',self.the_data.isnull().sum())
+        print('\n')
+        print(self.the_data.head())
+        print('punk mofo')
+
+        headers=[]
+        the_types=[]
+        no_null_count=[]
+        is_null_count=[]
+        headers=self.the_data.columns.tolist()
+        for i in headers:
+            the_types.append(self.the_data[i].dtypes)
+            no_null_count.append(self.the_data[i].notnull().sum())
+            is_null_count.append(self.the_data[i].isnull().sum())
+
+        info_data = {'headers': headers, 'data_types': the_types, 'non_null_count': no_null_count,'is_null_count': is_null_count}
+        self.the_info=pd.DataFrame.from_dict(info_data)
+       
+   
+
+
+# In[ ]:
+
+
+
+class clean_the_data(DataWorld):
+    
+    def data_clean(self):
+        """CLEANING THE HEADERS BY REMOVING SPACES & MAKING ALL LOWER CASE"""
+        #Removed the NaN columns
+
+        #All of the columns named UNUSED
+        p=self.the_data.loc[:, self.the_data.columns.str.contains('Unnamed')].head()
+        #Removing the UNUSED columns
+        self.the_data.drop(p,axis=1,inplace=True)
+        #replacing header spaces with underscores 
+        self.the_data.columns=self.the_data.columns.str.replace(' ','_')
+        #replacing header dashes with underscores
+        self.the_data.columns=self.the_data.columns.str.replace('-','_')
+        #replacing header dashes with underscores
+        self.the_data.columns=self.the_data.columns.str.replace('#','number')    
+        #make all headers lower case
+        self.the_data.columns=self.the_data.columns.str.lower()
+    
+        return self.the_data.info()
+    
+    def rename_this_column(self,old,new):
+        self.the_data.rename(columns={old:new},inplace=True)
+    
+    def convert_data_types(self,the_header,convert_to):
+        if (convert_to=='datetime'):
+            self.the_data[the_header]=pd.to_datetime(self.the_data[the_header])
+            #the_data[the_header] =  pd.to_datetime(the_data[the_header], format='%d%b%Y:%H:%M:%S.%f')
+            
+    def drop_these_columns(self,header_list):
+        print(header_list)
+        self.the_data.drop(header_list, axis=1,inplace=True)
+
+
+# In[ ]:
+
+
+class my_datasets(clean_the_data):
+    
+    def list_the_headers(self):
+        print(self.the_data.columns.tolist())
+    
+    def data_subset(self,the_header_list):
+        self.subset=self.the_data[the_header_list]
+        
+
+def check_unique(the_data,the_header):
+    the_count=the_data[the_header].value_counts(normalize=False)
+    the_percentage=the_data[the_header].value_counts(normalize=True)
+   
+    unique_test_areas=the_data[the_header].nunique()
+    print('Number of unique: ',unique_test_areas, '\n')
+    print(the_count,' \n')
+    print(the_percentage,' \n')
+    plot_title='Breakdown of '+the_header+' Data'
+    
+    if (unique_test_areas<=7):
+        pie_plot(the_count,plot_title)
+    elif(unique_test_areas>7):
+        Horz_Bar(the_data,the_header)
+
+
+def pie_plot(a,the_title):
+    labels=a.index
+    plt.figure(figsize=(8,8))
+    plt.title(the_title)
+    plt.pie(a,labels=labels, autopct='%1.1f%%',shadow=True, startangle=90)
+
+    plt.axis('tight')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    plt.show()
+
+
+def data_info(the_data):
+    
+    headers=[]
+    the_types=[]
+    no_null_count=[]
+    is_null_count=[]
+    headers=the_data.columns.tolist()
+    for i in headers:
+        the_types.append(the_data[i].dtypes)
+        no_null_count.append(the_data[i].notnull().sum())
+        is_null_count.append(the_data[i].isnull().sum())
+
+    info_data = {'headers': headers, 'data_types': the_types, 'non_null_count': no_null_count,'is_null_count': is_null_count}
+    the_info=pd.DataFrame.from_dict(info_data)
+    return the_info
+
+
+
+        
--- my_gui_maker.py
+++ my_gui_maker.py
@@ -4,7 +4,10 @@ from tkinter import *
 import os
 import sys
 import pandas as pd
+from pandas import set_option
 
+""" (1-11-19) This module contains the class for the Tkinter objects.
+It allows the user to build custom GUIs"""
 
 
 class my_window(Tk):
@@ -121,10 +124,54 @@ class My_Frame(Frame):
         self.list_box=Listbox(self,font=20,borderwidth=2,height=30,width=100)
         self.list_box.grid(row=rows,column=columns,columnspan=1,sticky=W)
         
-    def the_tree(self,rows,columns,head_cols):
-        self.my_tree=ttk.Treeview(self,height=20,columns=head_cols)
+##    def the_tree(self,rows,columns,head_cols):
+##        self.my_tree=ttk.Treeview(self,height=20,columns=head_cols)
+##        self.my_tree.bind("<Double-1>", self.OnDoubleClick)
+##        self.my_tree.grid(row=rows,column=columns)
+
+
+    tree_list=[]
+    def the_tree(self,rows,columns,df):
+
+        head_cols=tuple(df.columns.tolist())
+        self.my_tree=ttk.Treeview(self,columns=head_cols)
         self.my_tree.bind("<Double-1>", self.OnDoubleClick)
-        self.my_tree.grid(row=rows,column=columns)
+        self.tree_list.append(self.my_tree)
+        
+# Create header and column properties for the index
+        self.my_tree.heading('#0',text='Index',anchor=CENTER)
+        self.my_tree.column(column='#0',width=50,anchor='center')
+        
+        #     Creating the headers for the data 
+        for col_num,i in enumerate(head_cols,1):
+            col_index='#'+str(col_num)
+            self.my_tree.heading(col_index,text=i,anchor=CENTER)
+            self.my_tree.column(column=i,width=80,anchor='center')
+            
+        #     Creating (inserting) the index column values
+        for j in range(0,len(df)):
+            self.my_tree.insert('',str(j),'index'+str(j),text=str(j),tag='blue')
+            
+        #     Setting the data values to the cooresponding index
+            for k,sd in enumerate(head_cols,0):
+                self.my_tree.set(item='index'+str(j),column=sd,value=df[sd].iloc[j])
+        
+        
+        yscrollbar = ttk.Scrollbar(self, orient='vertical', command=self.my_tree.yview)
+        xscrollbar = ttk.Scrollbar(self, orient='horizontal', command=self.my_tree.xview)
+        self.my_tree['yscroll'] = yscrollbar.set
+        self.my_tree['xscroll'] = xscrollbar.set
+
+        self.my_tree.grid(row=rows,column=columns,sticky=W,columnspan=2)
+
+        yscrollbar.grid(row=rows, column=columns+1, sticky='ns',in_=self.the_frame)
+        xscrollbar.grid(row=rows+1, column=columns, sticky='we',in_=self.the_frame)
+        
+        yscrollbar.config(command=self.my_tree.yview)
+        xscrollbar.config(command=self.my_tree.xview)
+        
+        self.my_tree.configure(yscrollcommand = yscrollbar.set, xscrollcommand = xscrollbar.set, selectmode="browse")
+        
         
     def OnDoubleClick(self, event):
         item = self.my_tree.selection()[0]
@@ -132,8 +179,75 @@ class My_Frame(Frame):
         d=self.my_tree.item(item)
         print('Your header is  '+d['values'][1])
         print('The data type is  '+d['values'][0])
-    
 
+
+##    
+    def my_dlist_canvas(self,rows,columns,df):
+        self.dlist_canvas=Canvas(self,bd=0, highlightthickness=0,bg="yellow")
+        self.dlist_canvas.configure(width=500,height=500)
+
+        self.Lframe=Frame(self.dlist_canvas)
+        set_option('precision',3)
+        index_count=df.index.shape[0]
+        head_cols=tuple(df.columns.tolist())
+        col=0
+        for col_num,i in enumerate(head_cols,0):
+            self.dlist=Listbox(self.Lframe,width=15,height=1)
+            self.dlist.insert(END,i)
+            self.dlist.configure(bg="black",fg="white")
+            self.dlist.grid(row=1,column=col_num,sticky=W)
+            col=col+1
+        
+        for i in head_cols:
+            col_index=head_cols.index(i)
+            row_index=2
+#             print(col_index,row_index,len(df))
+            for p in range(len(df)):
+                self.dlist=Listbox(self.Lframe,width=15,height=1) 
+                data=df.iloc[p][col_index]
+                self.dlist.insert(END,data)
+                self.dlist.grid(row=row_index,column=col_index,sticky=W)
+                row_index=row_index+1
+
+##            print ('frame width: ',self.Lframe.winfo_width())
+        self.data_window=self.dlist_canvas.create_window(0,0,window=self.Lframe,anchor='nw',tags=('data_holder'))
+            
+        yscrollbar = ttk.Scrollbar(self, orient='vertical', command=self.dlist_canvas.yview)
+        xscrollbar = ttk.Scrollbar(self, orient='horizontal', command=self.dlist_canvas.xview)
+        
+        yscrollbar.pack(side=RIGHT,fill=Y)
+
+        self.dlist_canvas.config(yscrollcommand = yscrollbar.set)
+        self.dlist_canvas.config(xscrollcommand = xscrollbar.set)  
+        xscrollbar.pack(side=BOTTOM,fill=X)
+        
+        self.dlist_canvas.pack(side=LEFT)
+    
+        self.Lframe.bind("<Configure>", self.OnFrameConfigure)
+##        self.dlist_canvas.bind('<Configure>', self.FrameWidth)
+        
+
+    def FrameWidth(self, event):
+        canvas_width = event.width
+        self.dlist_canvas.itemconfig(self.data_window, width=canvas_width)
+##        self.dlist_canvas.config(width=canvas_width)
+
+    def OnFrameConfigure(self, event):
+        self.dlist_canvas.configure(scrollregion=self.dlist_canvas.bbox('data_holder'))
+#         print('the data_window bbox: ',self.dlist_canvas.bbox('data_holder'))
+
+
+    def adjust_index_geo(self, event):
+        """fixes the index canvas in a confined geometry"""
+        canvas_width = event.width
+        canvas_height=event.height
+        self.dlist_canvas.itemconfig(self.data_window, width=canvas_width, height=canvas_height)
+        self.dlist_canvas.config(width=canvas_width,height=canvas_height)
+
+
+
+
+                        
 
 
 
