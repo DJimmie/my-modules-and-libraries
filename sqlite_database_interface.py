40cfec85238462d677a7cdb085e8ceb883614545
DJimmie <dowdjimmie10@gmail.com>
2020-09-09 20:10:28 -0500

    Updates to sqlite_database_interface.py while working on the System-CSP systems.py program.

--- sqlite_database_interface.py
+++ sqlite_database_interface.py
@@ -13,9 +13,48 @@ from tkinter import messagebox
 ## FUNCTIONS-----------------FUNCTIONS-----------------FUNCTIONS
 
 def database_server():
-    # server='C:/Users/Crystal/Desktop/Programs/Systems-Database/hctc_systems_database.db'
-    server='C:/Users/JDowd/OneDrive - Schlumberger/Programming/TRIS-database/HCTC_TRIS_database.db'
+    server='C:/Users/Crystal/Desktop/Programs/Systems-Database/hctc_systems_database.db'
     return server
+
+
+def insert_new_record(table,columns,values):
+    
+    n=len(values[0])-1
+    the_sql_command = f"INSERT INTO {table} {columns} VALUES(?"+n*",?"+")"
+
+    print(the_sql_command)
+
+    conn = None
+
+    sqlite_server=database_server()
+    try:
+        conn=sqlite3.connect(sqlite_server)
+        cur=conn.cursor()
+        cur.executemany(the_sql_command,values) #cur.execute(the_sql_command) ----> execute() would not run a Transaction, so had to use executescript().
+        data = cur.fetchall()
+        conn.commit()
+        print ('Database Upload Complete')
+        messagebox.showinfo('Database Upload Complete','ITEM SUCCESSFULLY UPLOADED TO THE DATABASE.')
+    except Exception as error:
+        print('UNABLE TO ACCESS DATABASE. TRY AGAIN')
+        print(error)
+        messagebox.showerror('UNABLE TO ACCESS DATABASE','UNABLE TO ACCESS DATABASE: Server is not responding or query is invalid. Try Again or See Administrator.')
+
+def update_record (the_sql_command,values):
+
+    sqlite_server=database_server()
+    try:
+        conn=sqlite3.connect(sqlite_server)
+        cur=conn.cursor()
+        cur.execute(the_sql_command,values) 
+        data = cur.fetchall()
+        conn.commit()
+        print ('Database Upload Complete')
+        messagebox.showinfo('Database Upload Complete','ITEM SUCCESSFULLY UPLOADED TO THE DATABASE.')
+    except Exception as error:
+        print('UNABLE TO ACCESS DATABASE. TRY AGAIN')
+        print(error)
+
 
 def query_list(the_sql_command):
     """Query the database for combobox drop down values list"""
@@ -27,8 +66,10 @@ def query_list(the_sql_command):
         # query=executeTheCommand(the_sql_command) 
         header=query.columns[0]
         combo_box_list=list(query[header])
-    except:
+    except Exception as error:
         combo_box_list=['a','b','c']
+        print('UNABLE TO ACCESS DATABASE. TRY AGAIN')
+        print(error)
 
     return combo_box_list
 
