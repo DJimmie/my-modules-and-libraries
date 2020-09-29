57f9c9ddc082cbf84bac3c7164382d8760170639
DJimmie <dowdjimmie10@gmail.com>
2020-09-28 21:33:32 -0500

    Working my-modules-and-libraries Issue#2

---
+++ program_work_dir/program_work_dir.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import configparser
+
+
+def verify_client_folder():
+    """Check for the working directory. If does not exist, then call the class ClientFolder() that creates it."""
+
+    
+
+    # get name of the client program
+    client=os.path.basename(__file__)
+    client_folder_name=client.split('.')[0]
+    print(client_folder_name)
+
+    # search for the C:\\parent_folder\
+    verify=os.path.exists(f'C:\\{client_folder_name}')
+
+    print(verify)
+
+    # if found then return. if not found then call ClientFolder
+
+    if verify==False:
+        client_folder=os.makedirs(f'C:\\{client_folder_name}')
+        return client_folder
+    else:
+        return f'C:\\{client_folder_name}'
+
+
+
+    
+
+class ClientFolder():
+    """Creates the client software's parent folder as follows
+    C:\\parent_folder\\working_dir\\
+
+        -When possible the parent folder will take the name of the program (client) that is using program_work_dir.py.
+        -The working directories are created in the WorkDirectory class"""
+    pass
+
+class WorkDirectory():
+    """ This is the directory for configuartion files, data storage, temporary folders, etc. """
+    def __init__(self,client_folder):
+        self.client_folder=client_folder
+        print(self.client_folder)
+
+    def make(self,project):
+        os.makedirs(f'C:\\{client_folder_name}\\{project}')
+
+
+
+class ConfigFile():
+    """Creates client configuration (.ini) files as needed by the client software"""
+    pass
+
+
+x=verify_client_folder()
+
+WorkDirectory(x)
+
+WorkDirectory.make('jd1')
