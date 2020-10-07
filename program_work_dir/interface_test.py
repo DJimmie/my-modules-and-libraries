7fd8a028fc0d555494605faf220b64399ecac7a9
DJimmie <dowdjimmie10@gmail.com>
2020-10-07 09:38:25 -0500

    Task: create folder in a user specified directory. Folder shall have a name specified by the host program.
    created interface.test.py to test functionality of the pwd module.

---
+++ program_work_dir/interface_test.py
@@ -0,0 +1,6 @@
+import program_work_dir as pwd
+import os
+
+pwd.ClientFolder('fluffy')
+
+print(pwd.ClientFolder.client_folder)
--- program_work_dir/program_work_dir.py
+++ program_work_dir/program_work_dir.py
@@ -31,12 +31,40 @@ def verify_client_folder():
     
 
 class ClientFolder():
-    """Creates the client software's parent folder as follows
-    C:\\parent_folder\\working_dir\\
-
+    """Creates the client software's parent folder as follows 
+        -C:\\parent_folder\\working_dir\\
         -When possible the parent folder will take the name of the program (client) that is using program_work_dir.py.
         -The working directories are created in the WorkDirectory class"""
-    pass
+    
+    client_folder=None
+    def __init__(self,client):
+        # get name of the client program
+        self.client=client #os.path.basename(__file__)
+        self.client_folder_name=self.client.split('.')[0]
+        
+        ClientFolder.client_folder=self.verify_client_folder()
+    
+    def __str__(self):
+        return str(self.client_folder_name)
+    
+    def verify_client_folder(self):
+        """Check for the working directory. If does not exist, then call the class ClientFolder() that creates it."""
+
+        # search for the C:\\parent_folder\
+        verify=os.path.exists(f'C:\\{self.client_folder_name}')
+
+        print(verify)
+
+        # if found then return. if not found then call ClientFolder
+
+        if verify==False:
+            self.client_folder=os.makedirs(f'C:\\{self.client_folder_name}')
+            return self.client_folder
+        else:
+            return f'C:\\{self.client_folder_name}'
+
+
+
 
 class WorkDirectory():
     """ This is the directory for configuartion files, data storage, temporary folders, etc. """
@@ -54,8 +82,4 @@ class ConfigFile():
     pass
 
 
-x=verify_client_folder()
-
-WorkDirectory(x)
-
-WorkDirectory.make('jd1')
+# a=ClientFolder()
